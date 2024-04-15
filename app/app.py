a7ce7d69a6a3a7a5977e1e1253d187d8afd2e99b
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2024-04-15 10:31:14 -0400

    Provide documentation
    Comment the steps taken to reach our output

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+# Imports
 import seaborn as sns
 from faicons import icon_svg
 
@@ -5,76 +6,109 @@ from shiny import reactive
 from shiny.express import input, render, ui
 import palmerpenguins 
 
+# Load penguins data into a pandas data frame
 df = palmerpenguins.load_penguins()
 
+# Page options
 ui.page_opts(title="Penguins dashboard", fillable=True)
 
-
+# === Sidebar =====================================================================
 with ui.sidebar(title="Filter controls"):
-    ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+    # Add a slider
+    ui.input_slider("mass", "Mass (g)", 2000, 6000, 6000)
+
+    # Add a grouped checkbox
     ui.input_checkbox_group(
         "species",
         "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
+
+    # Add in a horzontal rule for aesthetics
     ui.hr()
+
+    # Provide some helpful links
+    #-> header to inform reader
     ui.h6("Links")
+
+    #-> GitHub source page
     ui.a(
         "GitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
         target="_blank",
     )
+
+    #-> GitHub app
     ui.a(
         "GitHub App",
         href="https://denisecase.github.io/cintel-07-tdash/",
         target="_blank",
     )
+
+    #-> GitHub issues page
     ui.a(
         "GitHub Issues",
         href="https://github.com/denisecase/cintel-07-tdash/issues",
         target="_blank",
     )
+
+    #-> Shiny homepage
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+
+    #-> Shiny basic dashboard example
     ui.a(
         "Template: Basic Dashboard",
         href="https://shiny.posit.co/py/templates/dashboard/",
         target="_blank",
     )
+
+    #-> GitHub dashboard example
     ui.a(
         "See also",
         href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
         target="_blank",
     )
 
-
+# === Main =========================================================================
+# --- Numeric Outputs --------------------------------------------------------------
 with ui.layout_column_wrap(fill=False):
+    # Add value boxes
+    #-> Number of penguins
     with ui.value_box(showcase=icon_svg("earlybirds")):
         "Number of Documented Penguins"
 
+        # Function to return the number of penguins
         @render.text
         def count():
             return filtered_df().shape[0]
 
+    #-> Bill L=length
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
         "Average Bill Length"
 
+        # Function to return the average bill length
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
+    #-> Bill depth
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
         "Average Bill Height"
 
+        # Function to return the average bill depth
         @render.text
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
-
+# --- Scatterplot ------------------------------------------------------------------
 with ui.layout_columns():
+    # Create a card for the scatterplot
     with ui.card(full_screen=True):
+        # Add a header to the card
         ui.card_header("Bill length and depth")
 
+        # Funcdtion to create the scatterplot
         @render.plot
         def length_depth():
             return sns.scatterplot(
@@ -84,9 +118,12 @@ with ui.layout_columns():
                 hue="species",
             )
 
+    # Create a card for the summary
     with ui.card(full_screen=True):
+        # Add a header to the card
         ui.card_header("Penguins Data")
 
+        # Function to return the filtered data to summarize
         @render.data_frame
         def summary_statistics():
             cols = [
@@ -98,10 +135,7 @@ with ui.layout_columns():
             ]
             return render.DataGrid(filtered_df()[cols], filters=True)
 
-
-#ui.include_css(app_dir / "styles.css")
-
-
+# === Function to filter data =====================================================
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
