eb5804c5e1975f12794ce19d78abe5829e14e90d
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2024-04-15 10:19:06 -0400

    Customize display text
    Change value box titles

--- app/app.py
+++ app/app.py
@@ -50,21 +50,21 @@ with ui.sidebar(title="Filter controls"):
 
 with ui.layout_column_wrap(fill=False):
     with ui.value_box(showcase=icon_svg("earlybirds")):
-        "Number of penguins"
+        "Number of Documented Penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
 
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
-        "Average bill length"
+        "Average Bill Length"
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
-        "Average bill depth"
+        "Average Bill Height"
 
         @render.text
         def bill_depth():
